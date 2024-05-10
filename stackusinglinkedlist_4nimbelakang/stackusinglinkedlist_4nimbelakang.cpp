1464f938e40082efbea8da9321eb3de3de568e4c
Iaannn06 <alfianazad05@gmail.com>
2024-05-10 11:24:55 +0700

    membuat case ketiga

--- stackusinglinkedlist_4nimbelakang/stackusinglinkedlist_4nimbelakang.cpp
+++ stackusinglinkedlist_4nimbelakang/stackusinglinkedlist_4nimbelakang.cpp
@@ -79,4 +79,12 @@ int main() {
 			}
 			break;
 		}
+		case 3:
+			if (!stack.isEmpty()) {
+				stack.peek();
+			}
+			else {
+				cout << "Stack is empty. No top value." << endl;
+			}
+			break;
 	}
